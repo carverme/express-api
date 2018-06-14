82185715fc55c9b243b3038ffd405dda99d54f4c
Matt Carver <mattcarver.wd@gmail.com>
2018-06-13 18:05:19 -0700

    added get, put, and delete

--- index.js
+++ index.js
@@ -8,13 +8,57 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', 'ejs');
-app.use(bp.urlencoded({extended: false}));
+app.use(bp.urlencoded({extended: true}));
 app.use(ejsLayouts);
 
 //get vehicles, returns all vehicles
 app.get('/vehicles', function(req, res) {
   var vehicles = fs.readFileSync('./data.json');
   vehicles = JSON.parse(vehicles);
+  res.json(vehicles);
+});
+
+//POST /vehicles - adds a new vehicle.
+app.post('/vehicles', function(req, res) {
+  console.log(req.body);
+  var vehicles = fs.readFileSync('./data.json');
+  vehicles = JSON.parse(vehicles);
+  vehicles.push( {make: req.body.make, model: req.body.model} );
+  fs.writeFileSync('./data.json', JSON.stringify(vehicles));
+  res.json(vehicles);
+});
+
+
+app.get('/vehicles/:id', function(req, res) {
+  var vehicles = fs.readFileSync('./data.json');
+  vehicles = JSON.parse(vehicles);
+  var vehicleIndex = req.params.id;
+  if (vehicleIndex >= vehicles.length) {
+      res.json({make: null, model: null});
+  } else {
+    res.json(vehicles[vehicleIndex]);
+  }
+});
+
+app.put('/vehicles/:id', function(req, res) {
+  var vehicles = fs.readFileSync('./data.json');
+  vehicles = JSON.parse(vehicles);
+  var vehicleIndex = req.params.id;
+  vehicles[vehicleIndex].make = req.body.make;
+  vehicles[vehicleIndex].model = req.body.model;
+  fs.writeFileSync('./data.json', JSON.stringify(vehicles));
+  res.json(vehicles);
+  if (vehicleIndex >= vehicles.length) {
+    res.json({make: null, model: null});
+  }
+});
+
+app.delete('/vehicles/:id', function(req, res) {
+  var vehicles = fs.readFileSync('./data.json');
+  vehicles = JSON.parse(vehicles);
+  var vehicleIndex = req.params.id;
+  vehicles.splice( vehicleIndex, 1);
+  fs.writeFileSync('./data.json', JSON.stringify(vehicles));
   res.json(vehicles);
 });
 
